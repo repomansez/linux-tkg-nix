64f70a64b9dd3b7bd927a54da4e6173c54daf66c
repomansez <sbctani@protonmail.com>
2023-04-27 17:30:51 -0300

    mister proper

--- include/generated/compile.h
+++
@@ -1,4 +0,0 @@
-#define UTS_MACHINE		"x86_64"
-#define LINUX_COMPILE_BY	"root"
-#define LINUX_COMPILE_HOST	"fish"
-#define LINUX_COMPILER		"gcc (Gentoo 12.2.1_p20230304 p13) 12.2.1 20230304, GNU ld (Gentoo 2.40 p4) 2.40.0"
